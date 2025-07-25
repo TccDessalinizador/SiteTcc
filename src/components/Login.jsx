ba4d7adc64947c1f19103073b6c89c51111be0a6
EduardoMVasconcelos <eduardo.m.vasconcelos@eaportal.org>
2025-07-25 20:43:51 -0300

    html login pronto (falta toggle)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,23 @@ export function Login() {
     return (
         <div>
             <img src="../assets/logo tcc-black 1.png" alt="" />
+
+            <form action="">
+                <label>Login</label>
+                <input type="email" placeholder="Email ou telefone" required />
+
+                <label>Senha</label>
+                <input type="email" placeholder="Insira a senha" required />
+
+                {/* Falta o toggle aqui */}
+                <p>Lembre-me</p>
+
+                <button>Login</button>
+
+                <a href="#">Esqueceu a senha?</a>
+
+                <p>Não tem uma conta? <a href="#">Registre-se</a></p>
+            </form>
         </div>
     )
 }
